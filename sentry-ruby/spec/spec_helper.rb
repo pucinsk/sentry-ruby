629e37e622856f4e9a93e7cab43168e80ee23bba
st0012 <stan001212@gmail.com>
2022-03-24 12:17:26 +0000

    Provide helpers to setup or teardown Sentry tests

--- sentry-ruby/spec/spec_helper.rb
+++ sentry-ruby/spec/spec_helper.rb
@@ -23,6 +23,32 @@ module Sentry
   module TestHelper
     DUMMY_DSN = 'http://12345:67890@sentry.localdomain/sentry/42'
 
+    def setup_sentry_test(&block)
+      raise "please make sure the SDK is initialized for testing" unless Sentry.initialized?
+      copied_config = Sentry.configuration.dup
+      # configure dummy DSN, so the events will not be sent to the actual service
+      copied_config.dsn = DUMMY_DSN
+      # set transport to DummyTransport, so we can easily intercept the captured events
+      copied_config.transport.transport_class = Sentry::DummyTransport
+      # make sure SDK allows sending under the current environment
+      copied_config.enabled_environments << copied_config.environment
+
+      # user can overwrite some of the configs, with a few exceptions like:
+      # - capture_exception_frame_locals
+      # - auto_session_tracking
+      block.call(copied_config)
+
+      test_client = Sentry::Client.new(copied_config)
+      Sentry.get_current_hub.bind_client(test_client)
+    end
+
+    def teardown_sentry_test
+      return unless Sentry.initialized?
+
+      sentry_transport.events = []
+      sentry_transport.envelopes = []
+    end
+
     def sentry_transport
       Sentry.get_current_client.transport
     end
